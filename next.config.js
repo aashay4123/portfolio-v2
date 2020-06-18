c4b5751c390683139d57f5d424aad69115a17531
Aashay <aashayphirke5798@gmail.com>
2020-06-19 02:43:13 +0530

    Pending PWA

--- next.config.js
+++ next.config.js
@@ -2,41 +2,30 @@ const withSass = require("@zeit/next-sass");
 const withCSS = require("@zeit/next-css");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
-const withPWA = require("next-pwa");
-
-module.exports = withPWA(
-  withCSS(
-    withSass({
-      webpack(config, { dev }) {
-        config.module.rules.push({
-          test: /\.(eot|woff|woff2|ttf|mp4|webm|svg|png|jpg|gif)$/,
-          use: {
-            loader: "url-loader",
-            options: {
-              limit: 100000,
-            },
-            loader: "file-loader",
-            options: {
-              esModule: false,
-              outputPath: "./static/css/",
-              publicPath: "./",
-            },
+module.exports = withCSS(
+  withSass({
+    webpack(config, { dev }) {
+      config.module.rules.push({
+        test: /\.(eot|woff|woff2|ttf|mp4|webm|svg|png|jpg|gif)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 100000,
           },
-        });
-        if (config.mode === "production") {
-          if (Array.isArray(config.optimization.minimizer)) {
-            config.optimization.minimizer.push(new OptimizeCSSAssetsPlugin({}));
-          }
+          loader: "file-loader",
+          options: {
+            esModule: false,
+            outputPath: "./static/css/",
+            publicPath: "./",
+          },
+        },
+      });
+      if (config.mode === "production") {
+        if (Array.isArray(config.optimization.minimizer)) {
+          config.optimization.minimizer.push(new OptimizeCSSAssetsPlugin({}));
         }
-        return config;
-      },
-      pwa: {
-        // disable: process.env.NODE_ENV === "development",
-        register: true,
-        scope: "/",
-        dest: "/public",
-        sw: "/static/sw",
-      },
-    })
-  )
+      }
+      return config;
+    },
+  })
 );
